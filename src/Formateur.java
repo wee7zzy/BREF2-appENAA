bd9e74d60fe99466231154a6b6dc05f19e3f1697
wee7zzy <yassaniq@gmail.com>
2024-12-22 13:44:13 +0100

    CREATION DU CONSTRUCTEUR ET LES METHODES SETTERS AND GETTERS

--- src/Formateur.java
+++ src/Formateur.java
@@ -3,5 +3,28 @@ import java.util.Scanner;
 
 public class Formateur extends Personne{
 
+    static Scanner scanner=new Scanner(System.in);
+
+
+    private String specialite;
+    private Double salaire;
+    private Classe classe;
+
+    public Formateur(String nom, String prenom, String email, int id, String specialite, Double salaire) {
+        super(nom, prenom, email, id);
+        this.specialite = specialite;
+        this.salaire = salaire;
+        this.classe = classe;
+    }
+
+
+    public Double getSalaire() {
+        return salaire;
+    }
+
+    public void setSalaire(Double salaire) {
+        this.salaire = salaire;
+    }
+
 
 }
