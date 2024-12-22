5cd55ca36fdbde16177dd37c45d6b34c5a09ea26
wee7zzy <yassaniq@gmail.com>
2024-12-22 14:02:40 +0100

    declaration des varibles et des tableaux pour stocker les classes les formateurs les apprenants

--- src/Application.java
+++ src/Application.java
@@ -4,5 +4,16 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
 
+        int choix;
+
+        Scanner scanner=new Scanner(System.in);
+
+        ArrayList<Apprenant> apprenant=new ArrayList<>();
+        ArrayList<Formateur> formateur=new ArrayList<>();
+        ArrayList<Classe> classe=new ArrayList<>();
+
+
+
+
     }
 }
