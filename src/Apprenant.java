6003420b35e3a8da2f96ede2ac9a8c45941782ac
wee7zzy <yassaniq@gmail.com>
2024-12-22 13:21:52 +0100

    CLASSE APPRENANT J'AI FAI L'HERITAGE DE LA CALSSE PERSSONE J'AI AJOUTER LES DEUX VARIABLES NOTES ET CREER UN TABLEAU POUR STOCKER LES APPRENANTS

--- src/Apprenant.java
+++ src/Apprenant.java
@@ -1,7 +1,22 @@
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class Apprenant extends Personne{
+
+    static Scanner scanner=new Scanner(System.in);
+
+    private double notes;
+    public ArrayList<Apprenant> apprenants;
+
+
+    public Apprenant(String nom, String prenom, String email, int id, double notes) {
+        super(nom, prenom, email, id);
+        this.notes = notes;
+    }
+
+    public void setNotes(double notes) {
+        this.notes = notes;
+    }
+
 
 }
